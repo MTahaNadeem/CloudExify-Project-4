5ac8e7f38bacee093695a481b7b0ca3dc356cb43
Muhammad Taha Nadeem <tahanadeem478@gmail.com>
2026-08-12 17:03:29 +0500

    Supabase.js update

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = '[SUPABASE_URL]';
-const SUPABASE_ANON_KEY = '[SUPABASE_ANON_KEY]';
+const SUPABASE_URL = 'https://nekmitkyuhkaicbcqwai.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_uvtp6SvNk38AlINUFbQPEg_3tk7YXQL';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
